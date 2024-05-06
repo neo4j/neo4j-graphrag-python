1e556a07f726e8ceea51af01a81dbd6acfeafbef
estelle <estelle.scifo@neo4j.com>
2024-05-06 18:04:56 +0200

    Back to the normal dimension size in e2e tests

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -60,7 +60,7 @@ def setup_neo4j(driver):
         vector_index_name,
         label="Document",
         property="propertyKey",
-        dimensions=10,
+        dimensions=1536,
         similarity_fn="euclidean",
     )
 
@@ -70,7 +70,7 @@ def setup_neo4j(driver):
     )
 
     # Insert 10 vectors and authors
-    vector = [random.random() for _ in range(10)]
+    vector = [random.random() for _ in range(1536)]
 
     def random_str(n: int) -> str:
         return "".join([random.choice(string.ascii_letters) for _ in range(n)])
