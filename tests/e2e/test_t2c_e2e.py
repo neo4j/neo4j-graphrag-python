a7483b798fe45cc03b371b0f831a330d14662566
Alex Thomas <alexthomas93@users.noreply.github.com>
2024-05-21 14:54:00 +0100

    Added an end to end test for the text to cypher retriever

---
+++ tests/e2e/test_t2c_e2e.py
@@ -0,0 +1,27 @@
+import neo4j
+import pytest
+from neo4j_genai import TextToCypherRetriever
+
+
+@pytest.mark.usefixtures("setup_neo4j")
+def test_t2c_retriever_search(driver, llm):
+    t2c_query = """
+    MATCH (p:Person {name: "Hugo Weaving"})-[:ACTED_IN]->(m:Movie)
+    RETURN m.title
+    """
+    retriever = TextToCypherRetriever(driver=driver, llm=llm)
+    retriever.llm.invoke.return_value = t2c_query
+    query_text = "Which movies did Hugo Weaving star in?"
+    results = retriever.search(query_text=query_text)
+    assert isinstance(results, list)
+    assert len(results) == 5
+    for result in results:
+        assert isinstance(result, neo4j.Record)
+        assert "m.title" in result.keys()
+    assert set([result["m.title"] for result in results]) == {
+        "Cloud Atlas",
+        "V for Vendetta",
+        "The Matrix Revolutions",
+        "The Matrix Reloaded",
+        "The Matrix",
+    }
