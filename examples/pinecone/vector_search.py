7c302505bddf543b55d561f8f49512ea087985e2
Alex Thomas <alexthomas93@users.noreply.github.com>
2024-06-05 17:27:55 +0100

    Add external vector index support for Pinecone (#52)
    * Added pinecone-client to poetry
    
    * Copied weaviate populate_dbs.py to pinecone_e2e
    
    * Updated tests/e2e/pinecone_e2e/populate_dbs.py to work with Pinecone
    
    * Copied the Weaviate retriever to use as a base for the Pinecone retriever
    
    * Added first draft of PineconeNeo4jRetriever
    
    * Added input validation to PineconeNeo4jRetriever
    
    * Renamed PineconeModel to PineconeClientModel
    
    * Added Pinecone retriever unit tests
    
    * Added E2E tests for pinecone retriever
    
    * Updated pinecone retriever to work with new return types
    
    * Updated E2E tests to work with new types
    
    * Added __init__.py to E2E tests
    
    * Update src/neo4j_genai/retrievers/external/pinecone/pinecone.py
    
    Co-authored-by: willtai <william.tai@neo4j.com>
    
    * Updated Pinecone retriever with new Exceptions
    
    * Added Pinecone example scripts
    
    * Moved Pinecone Pydantic types to their own file
    
    * Ran import organizer on examples/weaviate/text_search_local_embedder.py
    
    * Added docs for PineconeNeo4jRetriever
    
    * Pinecone E2E test fix
    
    * Pinecone E2E test update
    
    * Pincone E2E test fix
    
    * Moved populate_neo4j to its own file
    
    * Moved populate_neo4j to utils
    
    * Updated PineconeNeo4jRetriever import in test_pinecone_e2e.py
    
    * Removed copyright from Pinecone and Weaviate examples
    
    * Refactored E2E test utils
    
    * Small fix to tests/e2e/utils.py
    
    * Ran poetry lock --no-update
    
    * Small formatting changes
    
    * More formatting changes
    
    * Moved embedding_biology.py to the examples folder
    
    * mypy fixes
    
    * Removed unused imports
    
    * More mypy fixes
    
    * More mypy fixes
    
    * Added .venv to .gitignore
    
    * Fixed mypy pytest conflict
    
    ---------
    
    Co-authored-by: willtai <william.tai@neo4j.com>

---
+++ examples/pinecone/vector_search.py
@@ -0,0 +1,27 @@
+from neo4j import GraphDatabase
+from neo4j_genai.retrievers.external.pinecone import PineconeNeo4jRetriever
+from pinecone import Pinecone
+
+from examples.embedding_biology import EMBEDDING_BIOLOGY
+
+NEO4J_AUTH = ("neo4j", "password")
+NEO4J_URL = "neo4j://localhost:7687"
+PC_API_KEY = "API_KEY"
+
+
+def main() -> None:
+    with GraphDatabase.driver(NEO4J_URL, auth=NEO4J_AUTH) as neo4j_driver:
+        pc_client = Pinecone(PC_API_KEY)
+        retriever = PineconeNeo4jRetriever(
+            driver=neo4j_driver,
+            client=pc_client,
+            index_name="jeopardy",
+            id_property_neo4j="id",
+        )
+
+        res = retriever.search(query_vector=EMBEDDING_BIOLOGY, top_k=2)
+        print(res)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/e2e/pinecone_e2e/test_pinecone_e2e.py
@@ -0,0 +1,131 @@
+#  Copyright (c) "Neo4j"
+#  Neo4j Sweden AB [https://neo4j.com]
+#  #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  #
+#      https://www.apache.org/licenses/LICENSE-2.0
+#  #
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import re
+from typing import Any, Generator
+from unittest import mock
+from unittest.mock import MagicMock
+
+import pytest
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from neo4j_genai import PineconeNeo4jRetriever
+from neo4j_genai.embedder import Embedder
+from neo4j_genai.types import RetrieverResult, RetrieverResultItem
+from pinecone import Pinecone
+
+from ..utils import EMBEDDING_BIOLOGY, build_data_objects, populate_neo4j
+
+
+@pytest.fixture(scope="module")
+def sentence_transformer_embedder() -> Generator[HuggingFaceEmbeddings, Any, Any]:
+    embedder = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    yield embedder
+
+
+@pytest.fixture(scope="module")
+def client() -> MagicMock:
+    return MagicMock(spec=Pinecone)
+
+
+@pytest.fixture(scope="module")
+def populate_neo4j_db(driver: MagicMock) -> None:
+    driver.execute_query("MATCH (n) DETACH DELETE n")
+    neo4j_objects, _ = build_data_objects("pinecone")
+    populate_neo4j(driver, neo4j_objects)
+
+
+@pytest.mark.usefixtures("populate_neo4j_db")
+def test_pinecone_neo4j_vector_input(driver: MagicMock, client: MagicMock) -> None:
+    retriever = PineconeNeo4jRetriever(
+        driver=driver, client=client, index_name="jeopardy", id_property_neo4j="id"
+    )
+    with mock.patch.object(retriever, "index") as mock_index:
+        top_k = 2
+        mock_index.query.return_value = {
+            "matches": [
+                {
+                    "id": "question_c458c6f64d8d47429636bc5a94c97f51",
+                    "score": 0.232427984,
+                    "values": [],
+                },
+                {
+                    "id": "question_3d53154d16068c1e86e024923bc220d8",
+                    "score": 0.184265107,
+                    "values": [],
+                },
+            ],
+            "namespace": "",
+            "usage": {"read_units": 5},
+        }
+
+        results = retriever.search(query_vector=EMBEDDING_BIOLOGY, top_k=top_k)
+
+        assert isinstance(results, RetrieverResult)
+        assert len(results.items) == top_k
+        for result in results.items:
+            assert isinstance(result, RetrieverResultItem)
+        pattern = (
+            r"<Record node=<Node element_id='.+' "
+            "labels=frozenset\({'Question'}\) properties={'question': 'In 1953 Watson \& "
+            "Crick built a model of the molecular structure of this, the gene-carrying "
+            "substance', 'id': 'question_c458c6f64d8d47429636bc5a94c97f51'}> "
+            r"score=0.232427984>"
+        )
+        assert re.match(pattern, results.items[0].content)
+
+
+@pytest.mark.usefixtures("populate_neo4j_db")
+def test_pinecone_neo4j_text_input(
+    driver: MagicMock, client: MagicMock, sentence_transformer_embedder: Embedder
+) -> None:
+    retriever = PineconeNeo4jRetriever(
+        driver=driver,
+        client=client,
+        index_name="jeopardy",
+        id_property_neo4j="id",
+        embedder=sentence_transformer_embedder,
+    )
+    with mock.patch.object(retriever, "index") as mock_index:
+        top_k = 2
+        mock_index.query.return_value = {
+            "matches": [
+                {
+                    "id": "question_c458c6f64d8d47429636bc5a94c97f51",
+                    "score": 0.232427984,
+                    "values": [],
+                },
+                {
+                    "id": "question_3d53154d16068c1e86e024923bc220d8",
+                    "score": 0.184265107,
+                    "values": [],
+                },
+            ],
+            "namespace": "",
+            "usage": {"read_units": 5},
+        }
+
+        results = retriever.search(query_text="biology", top_k=top_k)
+
+        assert isinstance(results, RetrieverResult)
+        assert len(results.items) == top_k
+        for result in results.items:
+            assert isinstance(result, RetrieverResultItem)
+        pattern = (
+            r"<Record node=<Node element_id='.+' "
+            "labels=frozenset\({'Question'}\) properties={'question': 'In 1953 Watson \& "
+            "Crick built a model of the molecular structure of this, the gene-carrying "
+            "substance', 'id': 'question_c458c6f64d8d47429636bc5a94c97f51'}> "
+            r"score=0.232427984>"
+        )
+        assert re.match(pattern, results.items[0].content)
