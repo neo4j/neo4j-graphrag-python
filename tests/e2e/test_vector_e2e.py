865f58dec0d74c57ba1ee6caa05fbdea67da3fe4
Alex Thomas <alexthomas93@users.noreply.github.com>
2024-06-07 15:43:39 +0100

    Stopped the embedding from being returned with VectorRetriever

--- tests/e2e/test_vector_e2e.py
+++ tests/e2e/test_vector_e2e.py
@@ -32,6 +32,7 @@ def test_vector_retriever_search_text(
     assert isinstance(results, RetrieverResult)
     assert len(results.items) == 5
     for result in results.items:
+        assert retriever._embedding_node_property not in result.content
         assert isinstance(result, RetrieverResultItem)
 
 
@@ -66,6 +67,7 @@ def test_vector_retriever_search_vector(driver: Driver) -> None:
     assert isinstance(results, RetrieverResult)
     assert len(results.items) == 5
     for result in results.items:
+        assert retriever._embedding_node_property not in result.content
         assert isinstance(result, RetrieverResultItem)
 
 
