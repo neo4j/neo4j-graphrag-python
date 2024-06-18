01690f6ec77f022bce4a4cf1be7fd8769b8d558b
Alex Thomas <alexthomas93@users.noreply.github.com>
2024-06-18 14:21:18 +0100

    Reverted test_weaviate_neo4j_text_input_remote_embedder

--- tests/e2e/weaviate_e2e/test_weaviate_e2e.py
+++ tests/e2e/weaviate_e2e/test_weaviate_e2e.py
@@ -129,6 +129,6 @@ def test_weaviate_neo4j_text_input_remote_embedder(
         r"labels=frozenset\({'Question'}\) properties={'question': 'In 1953 Watson \& "
         "Crick built a model of the molecular structure of this, the gene-carrying "
         "substance', 'id': 'question_c458c6f64d8d47429636bc5a94c97f51'}> "
-        r"score=0.6[0-9]+>"
+        r"score=0.5[0-9]+>"
     )
     assert re.match(pattern, results.items[0].content)
