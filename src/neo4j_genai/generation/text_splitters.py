69f3034223581bb3b51c647305ee7142cf22f2e1
Alex Thomas <alexthomas93@users.noreply.github.com>
2024-07-22 16:31:35 +0100

    Added __future__ import to text_splitters.py for backwards compatibility of type hints

--- src/neo4j_genai/generation/text_splitters.py
+++ src/neo4j_genai/generation/text_splitters.py
@@ -11,6 +11,8 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+from __future__ import annotations
+
 from langchain_text_splitters import TextSplitter as LangChainTextSplitter
 from llama_index.core.node_parser import TextSplitter as LlamaIndexTextSplitter
 
