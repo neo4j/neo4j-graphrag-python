681b0a96ee2d5e577261f83c5f59a08c7e031bba
Alex Thomas <alexthomas93@users.noreply.github.com>
2024-08-15 16:19:53 +0100

    Added a timer to the KG creation pipeline

--- src/neo4j_genai/experimental/pipeline/pipeline.py
+++ src/neo4j_genai/experimental/pipeline/pipeline.py
@@ -18,6 +18,7 @@ import asyncio
 import enum
 import logging
 from datetime import datetime
+from timeit import default_timer
 from typing import Any, AsyncGenerator, Awaitable, Callable, Optional
 
 from pydantic import BaseModel, Field
@@ -118,7 +119,9 @@ class TaskPipelineNode(PipelineNode):
             if the task run successfully, None if the status update
             was unsuccessful.
         """
-        logger.debug(f"Running component {self.name} with {kwargs}")
+        logger.info(f"Running component {self.name}")
+        logger.debug(f"Component {self.name} arguments: {kwargs}")
+        start_time = default_timer()
         try:
             await self.set_status(RunStatus.RUNNING)
         except PipelineStatusUpdateError:
@@ -130,6 +133,8 @@ class TaskPipelineNode(PipelineNode):
             status=self.status,
             result=component_result,
         )
+        end_time = default_timer()
+        logger.info(f"Component {self.name} finished in {end_time - start_time}s")
         return run_result
 
     def validate_inputs_config(self, input_data: dict[str, Any]) -> None:
@@ -467,8 +472,12 @@ class Pipeline(PipelineGraph[TaskPipelineNode, PipelineEdge]):
             task.validate_inputs_config(data)
 
     async def run(self, data: dict[str, Any]) -> dict[str, Any]:
+        logging.info("Starting pipeline")
+        start_time = default_timer()
         self.validate_inputs_config(data)
         self.reinitialize()
         orchestrator = Orchestrator(self)
         await orchestrator.run(data)
+        end_time = default_timer()
+        logging.info(f"Pipeline finished in {end_time - start_time}s")
         return self._final_results.all()
