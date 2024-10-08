4d1e353836dab5f45ca75c42abc736ff15e7c9d2
estelle <estelle.scifo@neo4j.com>
2024-10-08 09:38:06 +0200

    Remove Cypher syntax warning log

--- src/neo4j_graphrag/__init__.py
+++ src/neo4j_graphrag/__init__.py
@@ -12,3 +12,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import logging
+
+# disable Cypher syntax deprecation warning for now
+logging.getLogger("neo4j.notifications").setLevel(logging.ERROR)
