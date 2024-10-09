4790e2c5565db18991a33d7a133bcc3364e30377
estelle <estelle.scifo@neo4j.com>
2024-10-09 13:42:43 +0200

    Format

--- examples/customize/llms/vertexai_llm.py
+++ examples/customize/llms/vertexai_llm.py
@@ -1,7 +1,6 @@
 from neo4j_graphrag.llm import LLMResponse, VertexAILLM
 from vertexai.generative_models import GenerationConfig
 
-
 generation_config = GenerationConfig(temperature=0.0)
 llm = VertexAILLM(
     model_name="gemini-1.5-flash-001",
