c672a6a50a9689826d6e382cbe922cd840e8a8d9
estelle <estelle.scifo@neo4j.com>
2024-10-09 13:40:52 +0200

    Simple KG builder example

---
+++ examples/build_graph/simple_kg_builder_from_text.py
@@ -0,0 +1,62 @@
+"""This example illustrates how to get started easily with the SimpleKGPipeline
+and ingest text into a Neo4j Knowledge Graph.
+"""
+
+import asyncio
+
+import neo4j
+from neo4j_graphrag.embeddings import OpenAIEmbeddings
+from neo4j_graphrag.experimental.pipeline.kg_builder import SimpleKGPipeline
+from neo4j_graphrag.experimental.pipeline.pipeline import PipelineResult
+from neo4j_graphrag.llm import LLMInterface
+from neo4j_graphrag.llm.openai_llm import OpenAILLM
+
+TEXT = """Albert Einstein was a German physicist born in 1879 who wrote many groundbreaking
+papers especially about general relativity and quantum mechanics.
+"""
+
+
+async def define_and_run_pipeline(
+    neo4j_driver: neo4j.Driver,
+    llm: LLMInterface,
+) -> PipelineResult:
+    # Instantiate Entity and Relation objects
+    entities = ["PERSON", "ORGANIZATION", "LOCATION"]
+    relations = ["SITUATED_AT", "INTERACTS", "LED_BY"]
+    potential_schema = [
+        ("PERSON", "SITUATED_AT", "LOCATION"),
+        ("PERSON", "INTERACTS", "PERSON"),
+        ("ORGANIZATION", "LED_BY", "PERSON"),
+    ]
+    # Create an instance of the SimpleKGPipeline
+    kg_builder = SimpleKGPipeline(
+        llm=llm,
+        driver=neo4j_driver,
+        embedder=OpenAIEmbeddings(),
+        entities=entities,
+        relations=relations,
+        potential_schema=potential_schema,
+    )
+
+    return await kg_builder.run_async(text=TEXT)
+
+
+async def main() -> PipelineResult:
+    llm = OpenAILLM(
+        model_name="gpt-4o",
+        model_params={
+            "max_tokens": 2000,
+            "response_format": {"type": "json_object"},
+        },
+    )
+    with neo4j.GraphDatabase.driver(
+        "bolt://localhost:7687", auth=("neo4j", "password")
+    ) as driver:
+        res = await define_and_run_pipeline(driver, llm)
+    await llm.async_client.close()
+    return res
+
+
+if __name__ == "__main__":
+    res = asyncio.run(main())
+    print(res)
