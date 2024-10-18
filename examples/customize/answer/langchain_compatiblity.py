3fba4525ea38361ef1433312892b3a1ddac44b8a
estelle <estelle.scifo@neo4j.com>
2024-10-18 10:58:24 +0200

    Fix openai example

--- examples/customize/answer/langchain_compatiblity.py
+++ examples/customize/answer/langchain_compatiblity.py
@@ -1,19 +1,20 @@
 """The LLM interface is compatible with LangChain chat API,
- so any LangChain implementation can be used. For instance,
- in GraphRAG:
+ so any LangChain implementation can be used. Same for embedders.
+ For instance, in GraphRAG:
 
 Requires OPENAI_API_KEY to be in the env var.
 """
 
 import neo4j
-from neo4j_graphrag.embeddings.openai import OpenAIEmbeddings
 from neo4j_graphrag.generation import GraphRAG
-from neo4j_graphrag.llm import OpenAILLM
 from neo4j_graphrag.retrievers import VectorCypherRetriever
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_openai.chat_models import ChatOpenAI
 
-URI = "neo4j://localhost:7687"
-AUTH = ("neo4j", "password")
-DATABASE = "neo4j"
+# Define database credentials
+URI = "neo4j+s://demo.neo4jlabs.com"
+AUTH = ("recommendations", "recommendations")
+DATABASE = "recommendations"
 INDEX = "moviePlotsEmbedding"
 
 
@@ -23,7 +24,7 @@ driver = neo4j.GraphDatabase.driver(
     database=DATABASE,
 )
 
-embedder = OpenAIEmbeddings()
+embedder = OpenAIEmbeddings(model="text-embedding-ada-002")
 
 retriever = VectorCypherRetriever(
     driver,
@@ -32,9 +33,12 @@ retriever = VectorCypherRetriever(
     embedder=embedder,  # type: ignore
 )
 
-llm = OpenAILLM(model_name="gpt-4o", model_params={"temperature": 0})
+llm = ChatOpenAI(model="gpt-4o", temperature=0)
 
-rag = GraphRAG(retriever=retriever, llm=llm)
+rag = GraphRAG(
+    retriever=retriever,
+    llm=llm,  # type: ignore
+)
 
 result = rag.search("Tell me more about Avatar movies")
 print(result.answer)
