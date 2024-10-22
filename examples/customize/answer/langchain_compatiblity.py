7bb7281cd1bdc4da74669a5a0d3b046cfdf950d1
Alex Thomas <alexthomas93@users.noreply.github.com>
2024-10-22 16:41:47 +0100

    Fixes mypy issue for execute_query function

--- examples/customize/answer/langchain_compatiblity.py
+++ examples/customize/answer/langchain_compatiblity.py
@@ -30,14 +30,14 @@ retriever = VectorCypherRetriever(
     driver,
     index_name=INDEX,
     retrieval_query="WITH node, score RETURN node.title as title, node.plot as plot",
-    embedder=embedder,  # type: ignore
+    embedder=embedder,
 )
 
 llm = ChatOpenAI(model="gpt-4o", temperature=0)
 
 rag = GraphRAG(
     retriever=retriever,
-    llm=llm,  # type: ignore
+    llm=llm,
 )
 
 result = rag.search("Tell me more about Avatar movies")
