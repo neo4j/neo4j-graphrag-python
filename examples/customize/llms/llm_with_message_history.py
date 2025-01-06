798794450ef0d199bdc5bbf043c6f4bb713b1812
estelle <estelle.scifo@neo4j.com>
2025-01-06 15:07:58 +0100

    Add examples for LLM message history and system instructions

---
+++ examples/customize/llms/llm_with_message_history.py
@@ -0,0 +1,38 @@
+"""This example illustrates the message_history feature
+of the LLMInterface by mocking a conversation between a user
+and an LLM about Tom Hanks.
+
+OpenAILLM can be replaced by any supported LLM from this package.
+"""
+from neo4j_graphrag.llm import LLMResponse, OpenAILLM
+
+# set api key here on in the OPENAI_API_KEY env var
+api_key = None
+# api_key = "sk-..."
+
+llm = OpenAILLM(model_name="gpt-4o", api_key=api_key)
+
+questions = [
+    "What are some movies Tom Hanks starred in?",
+    "Is he also a director?",
+    "Wow, that's impressive. And what about his personal life, does he have children?",
+]
+
+history = []
+for question in questions:
+    res: LLMResponse = llm.invoke(
+        question,
+        message_history=history,
+    )
+    history.append({
+        "role": "user",
+        "content": question,
+    })
+    history.append({
+        "role": "assistant",
+        "content": res.content,
+    })
+
+    print("#"*50, question)
+    print(res.content)
+    print("#"*50)
---
+++ examples/customize/llms/llm_with_system_instructions.py
@@ -0,0 +1,30 @@
+"""This example illustrates how to set system instructions for LLM.
+
+OpenAILLM can be replaced by any supported LLM from this package.
+"""
+from neo4j_graphrag.llm import LLMResponse, OpenAILLM
+
+# set api key here on in the OPENAI_API_KEY env var
+api_key = None
+# api_key = "sk-..."
+
+llm = OpenAILLM(
+    model_name="gpt-4o",
+    api_key=api_key,
+    system_instruction="Answer with a serious tone"
+)
+
+question = "How fast is Santa Claus during the Christmas eve?"
+
+res: LLMResponse = llm.invoke(
+    question,
+)
+print(res.content)
+
+
+# system instruction can be overwritten for each invoke:
+res: LLMResponse = llm.invoke(
+    question,
+    system_instruction="Answer with a joke"
+)
+print(res.content)
