83e4024a037ffde1d0bd74ffbdf8c6f430858f7f
estelle <estelle.scifo@neo4j.com>
2025-01-11 11:18:59 +0100

    Fix cohere test

--- tests/unit/llm/test_cohere_llm.py
+++ tests/unit/llm/test_cohere_llm.py
@@ -53,14 +53,14 @@ def test_cohere_llm_invoke_with_message_history_happy_path(mock_cohere: Mock) ->
     mock_cohere.ClientV2.return_value.chat.return_value = chat_response_mock
 
     system_instruction = "You are a helpful assistant."
-    llm = CohereLLM(model_name="something", system_instruction=system_instruction)
+    llm = CohereLLM(model_name="something")
     message_history = [
         {"role": "user", "content": "When does the sun come up in the summer?"},
         {"role": "assistant", "content": "Usually around 6am."},
     ]
     question = "What about next season?"
 
-    res = llm.invoke(question, message_history)  # type: ignore
+    res = llm.invoke(question, message_history, system_instruction=system_instruction)  # type: ignore
     assert isinstance(res, LLMResponse)
     assert res.content == "cohere response text"
     messages = [{"role": "system", "content": system_instruction}]
