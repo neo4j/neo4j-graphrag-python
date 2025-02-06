e905745163b8f9dc4b7ccee5330300d3fb124696
Alex Thomas <alexthomas93@users.noreply.github.com>
2025-02-06 10:45:56 +0000

    More reversions

--- src/neo4j_graphrag/retrievers/vector.py
+++ src/neo4j_graphrag/retrievers/vector.py
@@ -292,7 +292,7 @@ class VectorCypherRetriever(Retriever):
         )
         self.result_formatter = validated_data.result_formatter
         self._node_label = None
-        self._node_embedding_node_property = None
+        self._node_embedding_property = None
         self._embedding_dimension = None
         self._fetch_index_infos(self.index_name)
 
@@ -364,7 +364,7 @@ class VectorCypherRetriever(Retriever):
             search_type=SearchType.VECTOR,
             retrieval_query=self.retrieval_query,
             node_label=self._node_label,
-            embedding_node_property=self._node_embedding_node_property,
+            embedding_node_property=self._node_embedding_property,
             embedding_dimension=self._embedding_dimension,
             filters=filters,
         )
