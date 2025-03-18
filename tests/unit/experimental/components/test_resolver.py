13a89e6460e9f135a7bf972b812c1a09c463e127
nathaliecharbel <nathalie.charbel@neotechnology.com>
2025-03-18 10:16:28 +0100

    Add unit tests

--- tests/unit/experimental/components/test_resolver.py
+++ tests/unit/experimental/components/test_resolver.py
@@ -18,6 +18,7 @@ import neo4j
 import pytest
 from neo4j_graphrag.experimental.components.resolver import (
     SinglePropertyExactMatchResolver,
+    SpaCySemanticMatchResolver,
 )
 from neo4j_graphrag.experimental.components.types import ResolutionStats
 
@@ -57,3 +58,112 @@ async def test_simple_resolver_custom_filter(driver: MagicMock) -> None:
             )
         ]
     )
+    
+    
+@pytest.mark.asyncio
+async def test_spacy_resolver_match_on_name_property(driver: MagicMock) -> None:
+    driver.execute_query.side_effect = [
+        (
+            [
+                neo4j.Record(
+                    {
+                        "lab": "Person",
+                        "labelCluster": [
+                            {"id": 1, "name": "Alice"},
+                            {"id": 2, "name": "Alice"}
+                        ],
+                    }
+                )
+            ],
+            None,
+            None,
+        ),
+        (
+            [neo4j.Record({"id(node)": 1})],
+            None,
+            None,
+        ),
+    ]
+
+    resolver = SpaCySemanticMatchResolver(driver=driver)
+
+    res = await resolver.run()
+    assert isinstance(res, ResolutionStats)
+    assert res.number_of_nodes_to_resolve == 2
+    assert res.number_of_created_nodes == 1
+
+    assert driver.execute_query.call_count == 2
+
+@pytest.mark.asyncio
+async def test_spacy_resolver_no_merge(driver: MagicMock) -> None:
+    driver.execute_query.side_effect = [
+        (
+            [
+                neo4j.Record(
+                    {
+                        "lab": "Person",
+                        "labelCluster": [
+                            {"id": 1, "name": "Alice"},
+                            {"id": 2, "name": "Bob"}
+                        ],
+                    }
+                )
+            ],
+            None,
+            None,
+        ),
+    ]
+
+    resolver = SpaCySemanticMatchResolver(driver=driver)
+
+    res = await resolver.run()
+    assert res.number_of_nodes_to_resolve == 2
+    assert res.number_of_created_nodes == 0
+
+    assert driver.execute_query.call_count == 1
+
+@pytest.mark.asyncio
+async def test_spacy_resolver_match_on_multiple_text_properties(
+        driver: MagicMock
+) -> None:
+    driver.execute_query.side_effect = [
+        (
+            [
+                neo4j.Record(
+                    {
+                        "lab": "Person",
+                        "labelCluster": [
+                            {
+                                "id": 10,
+                                "name": "John Smith",
+                                "ssn": "23-45-6789"
+                            },
+                            {
+                                "id": 11,
+                                "name": "Jonathan Smith",
+                                "ssn": "23-45-6789"
+                            }
+                        ],
+                    }
+                )
+            ],
+            None,
+            None,
+        ),
+        (
+            [neo4j.Record({"id(node)": 10})],
+            None,
+            None,
+        ),
+    ]
+
+    resolver = SpaCySemanticMatchResolver(
+        driver=driver,
+        resolve_properties=["name", "ssn"]
+    )
+    res = await resolver.run()
+    assert isinstance(res, ResolutionStats)
+    assert res.number_of_nodes_to_resolve == 2
+    assert res.number_of_created_nodes == 1
+
+    assert driver.execute_query.call_count == 2
