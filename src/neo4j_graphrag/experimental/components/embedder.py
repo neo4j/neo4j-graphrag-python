bf24d0bd5c6843f9342a75ff97e20992f78d97e4
devatcadam <julien.baillet@cea.fr>
2026-01-22 15:19:02 +0100

    support async embedding for Embedder and Ollama
    Adding abstract function async_embed_query to class Embedder
    
    Making embedder Component behave like LLMEntityRelationExtractor for asynchronous call
    
    Implement async call for Ollama

--- src/neo4j_graphrag/experimental/components/embedder.py
+++ src/neo4j_graphrag/experimental/components/embedder.py
@@ -13,17 +13,18 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 from pydantic import validate_call
+import asyncio
 
 from neo4j_graphrag.embeddings.base import Embedder
 from neo4j_graphrag.experimental.components.types import TextChunk, TextChunks
 from neo4j_graphrag.experimental.pipeline.component import Component
-
 
 class TextChunkEmbedder(Component):
     """Component for creating embeddings from text chunks.
 
     Args:
         embedder (Embedder): The embedder to use to create the embeddings.
+        max_concurrency (int): The maximum number of concurrent tasks which can be used to make requests to the embedder. Default is 5.
 
     Example:
 
@@ -40,8 +41,13 @@ class TextChunkEmbedder(Component):
 
     """
 
-    def __init__(self, embedder: Embedder):
+    def __init__(
+        self, 
+        embedder: Embedder,
+        max_concurrency: int = 5
+    ):
         self._embedder = embedder
+        self.max_concurrency = max_concurrency
 
     def _embed_chunk(self, text_chunk: TextChunk) -> TextChunk:
         """Embed a single text chunk.
@@ -62,6 +68,28 @@ class TextChunkEmbedder(Component):
             metadata=metadata,
             uid=text_chunk.uid,
         )
+        
+    async def _async_embed_chunk(self, sem: asyncio.Semaphore, text_chunk: TextChunk) -> TextChunk:
+        """Asynchronously embed a single text chunk.
+
+        Args:
+            text_chunk (TextChunk): The text chunk to embed.
+            sem (asyncio.Semaphore): Semaphore to limit concurrency.
+
+        Returns:
+            TextChunk: The text chunk with an added "embedding" key in its
+            metadata containing the embeddings of the text chunk's text.
+        """
+        async with sem:
+            embedding = await self._embedder.async_embed_query(text_chunk.text)
+            metadata = text_chunk.metadata if text_chunk.metadata else {}
+            metadata["embedding"] = embedding
+            return TextChunk(
+                text=text_chunk.text,
+                index=text_chunk.index,
+                metadata=metadata,
+                uid=text_chunk.uid,
+            )
 
     @validate_call
     async def run(self, text_chunks: TextChunks) -> TextChunks:
@@ -73,6 +101,10 @@ class TextChunkEmbedder(Component):
         Returns:
             TextChunks: The input text chunks with each one having an added embedding.
         """
-        return TextChunks(
-            chunks=[self._embed_chunk(text_chunk) for text_chunk in text_chunks.chunks]
-        )
+        sem = asyncio.Semaphore(self.max_concurrency)
+        tasks = [
+            self._async_embed_chunk(sem, text_chunk) 
+            for text_chunk in text_chunks.chunks
+        ]
+        text_chunks: TextChunks = list(await asyncio.gather(*tasks))
+        return TextChunks(chunks=text_chunks)
