710325c05279c11be22241177d3166b9546cafb3
Zeljko Agic <zeljko.agic@gmail.com>
2026-06-16 10:42:26 +0200

    Add explain models

--- src/neo4j_graphrag/generation/__init__.py
+++ src/neo4j_graphrag/generation/__init__.py
@@ -1,4 +1,27 @@
+from .explain import (
+    ExplainConfig,
+    ExplainResult,
+    GraphContext,
+    GraphNodeRef,
+    GraphRelationshipRef,
+    GraphPath,
+    SourceRef,
+    TraceStep,
+)
 from .graphrag import GraphRAG
 from .prompts import PromptTemplate, RagTemplate, SchemaExtractionTemplate
 
-__all__ = ["GraphRAG", "PromptTemplate", "RagTemplate", "SchemaExtractionTemplate"]
+__all__ = [
+    "ExplainConfig",
+    "ExplainResult",
+    "GraphContext",
+    "GraphNodeRef",
+    "GraphPath",
+    "GraphRelationshipRef",
+    "GraphRAG",
+    "PromptTemplate",
+    "RagTemplate",
+    "SchemaExtractionTemplate",
+    "SourceRef",
+    "TraceStep",
+]
