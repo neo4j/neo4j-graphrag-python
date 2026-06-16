90147dede933a54b1f54d4ada8a570bbcd9ebc44
Zeljko Agic <zeljko.agic@gmail.com>
2026-06-16 10:46:00 +0200

    Revert "Add explain models"
    This reverts commit f72864324707332f6c5115c91827defa5b4773af.

--- src/neo4j_graphrag/generation/__init__.py
+++ src/neo4j_graphrag/generation/__init__.py
@@ -1,27 +1,4 @@
-from .explain import (
-    ExplainConfig,
-    ExplainResult,
-    GraphContext,
-    GraphNodeRef,
-    GraphRelationshipRef,
-    GraphPath,
-    SourceRef,
-    TraceStep,
-)
 from .graphrag import GraphRAG
 from .prompts import PromptTemplate, RagTemplate, SchemaExtractionTemplate
 
-__all__ = [
-    "ExplainConfig",
-    "ExplainResult",
-    "GraphContext",
-    "GraphNodeRef",
-    "GraphPath",
-    "GraphRelationshipRef",
-    "GraphRAG",
-    "PromptTemplate",
-    "RagTemplate",
-    "SchemaExtractionTemplate",
-    "SourceRef",
-    "TraceStep",
-]
+__all__ = ["GraphRAG", "PromptTemplate", "RagTemplate", "SchemaExtractionTemplate"]
